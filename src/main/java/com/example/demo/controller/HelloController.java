10cc3080481f76ea7081537251bb575ca9b4919a
dengmingzhu <dengmingzhu@skio.cn>
2019-11-26 17:54:33 +0800

    spirng简单实例

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+
+/**
+ * @Author DengMingZhu
+ * @Date 2019/10/31 15:51
+ * @Version 1.0
+ */
+@RestController
+public class HelloController {
+
+    //    返回cookie信息get接口
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(HttpServletResponse response) {
+        Cookie cookie = new Cookie("login", "true");
+        response.addCookie(cookie);
+        return "getCookes。。。";
+    }
+
+    //    客户端携带cookie访问
+    public String getWithCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (Objects.isNull(cookies)) {
+            return "必须携带cookie才能访问";
+        }
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("login") &&
+                    cookie.getValue().equals("true")) {
+                return "访问成功";
+            }
+        }
+        return "cookie访问";
+    }
+}
